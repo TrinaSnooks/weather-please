7a6c45bc6beb95ab51507e026d1b7256bd2b6811
Trina Snooks <trina.snooks@gmail.com>
2022-12-01 13:55:55 -0400

    added search field and test code for weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,5 @@
-/*
-var comment = document.getElementById("city");
-var saveButton = document.getElementById("search");
-var savedName = document.getElementById("saved-name");
 
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
-
-var searchResults= {
-  //student: student.value,
-  //grade: grade.value,
-  city: city.value.trim()
-};
-
-localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-renderMessage();
-
-});
-
-function renderMessage() {
-  var finalResults = JSON.parse(localStorage.getItem("searchResults"));
-  if (finalResults !== null) {
-    document.querySelector(".message").textcontent = finalResults.comment 
-  }
-}
-
------------------------------------------------
-*/
+//search form
 
 var comment = document.getElementById("msg");
 var saveButton = document.getElementById("save");
@@ -43,9 +17,68 @@ renderMessage();
 
 });
 
+//saved output of searches
+
+// HOW DO I SAVE TO LOCAL STORAGE OR PULL TO PAGE
+
 function renderMessage() {
-  var lastGrade = JSON.parse(localStorage.getItem("searchInput"));
-  if (lastGrade !== null) {
-    document.querySelector(".message").textContent = lastGrade.comment 
+  var finalSearch = JSON.parse(localStorage.getItem("searchInput"));
+  if (finalSearch !== null) {
+    document.querySelector(".message").textContent = finalSearch.comment 
   }
 }
+
+//---------------
+
+
+
+
+//fetch weather api
+
+// open weather API 
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//7eaa91188312f6b60b611deb90430946
+
+
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+
+//api.openweathermap.org. 
+
+//display location, date, temp, wind, humidity
+
+
+//display 5 day forecast in 5 blocks
+
+/*     call weather from open weather
+getData();
+
+async function getData() {
+    //http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}-->
+    const response = await fetch
+    
+    ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=7eaa91188312f6b60b611deb90430946')
+
+//then syntax
+
+
+    console.log(response);
+   /* const data = await response.json();
+    console.log(data);
+    length = data.drinks.length;
+    console.log(data);
+    var temp = "";
+    for (i = 0; i < length; i++) {
+        temp += "<tr>";
+        temp += "<td>" + data.drinks[i].strDrink + "</td>";
+        temp += "<td>" + data.drinks[i].strInstructions + "</td>";
+    }
+
+    document.getElementById("data").innerHTML = temp;
+    */
+}
+
+
+
+
